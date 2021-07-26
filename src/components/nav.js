1b36c5656d0b5771a4b2f120d2c140a7121b3702
Your Name <harrison.november@icloud.com>
2021-07-26 15:13:00 +0100

    commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -21,9 +21,14 @@ const Nav = () => {
     // useEffect(() => console.log("re-render because x changed:", modal), [modal])
 
     return (
-        <div className='fixed flex p-12 px-7 w-full items-center justify-center z-40'>
+        <div className='fixed flex p-4 px-7 w-full items-center justify-center z-40 bg-gray-900'>
             <div className='flex-1'>
-                <Link to={'/'} className='font-bold text-xl text-white text-opacity-40 hover:text-opacity-80 z-50 relative'>Stagelink</Link>
+                <Link to={'/'} className='flex font-bold text-xl text-white z-50 relative'>
+                    <div>
+                        <img className='w-5 h-full' alt='' src={require('./logo.svg').default}/>
+                    </div>
+                    <div>Stagelink</div>
+                    </Link>
             </div>
             <button onClick={() => setModal(!modal)} className='appearance-none bg-white bg-opacity-20 text-white rounded-2xl p-1.5 px-4'>
                 <svg className="w-4 h-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M2 6h20M2 12h20M2 18h20"></path></svg>
