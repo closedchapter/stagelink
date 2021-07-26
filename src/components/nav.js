a51e277f61edb23d5fddb0e9c40bacefc2278e83
Your Name <harrison.november@icloud.com>
2021-07-26 15:18:17 +0100

    commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -23,14 +23,15 @@ const Nav = () => {
     return (
         <div className='fixed flex p-4 px-7 w-full items-center justify-center z-40 bg-gray-900'>
             <div className='flex-1'>
-                <Link to={'/'} className='flex font-bold text-xl text-white z-50 relative'>
+                <Link to={'/'} className='flex font-bold text-xl text-white z-0 relative'>
                     <div>
                         <img className='w-5 h-full' alt='' src={require('./logo.svg').default}/>
                     </div>
-                    <div>Stagelink</div>
+                    <div>Stage</div>
+                    <div className=''>link</div>
                     </Link>
             </div>
-            <button onClick={() => setModal(!modal)} className='appearance-none bg-white bg-opacity-20 text-white rounded-2xl p-1.5 px-4'>
+            <button onClick={() => setModal(!modal)} className='appearance-none text-white rounded-2xl p-1.5 px-4'>
                 <svg className="w-4 h-full" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M2 6h20M2 12h20M2 18h20"></path></svg>
             </button>
             <div>
