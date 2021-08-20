7393bc1e9d1a202ecdfd6c9de9be5a958d7185a9
Your Name <harrison.november@icloud.com>
2021-08-20 18:08:08 +0100

    COmmit

--- src/Main/carousel.js
+++ src/Main/carousel.js
@@ -6,12 +6,12 @@ import AliWin from './images/hero.jpg'
 import Alisster from './images/imagery.jpg'
 
 const slides = [
-  { id: 0, url: 'https://images.unsplash.com/photo-1544511916-0148ccdeb877?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1901&q=80i&auto=format&fit=crop' },
+  { id: 0, url: AliFight },
+  { id: 3, url: Alisster },
+  { id: 2, url: 'https://images.unsplash.com/photo-1544511916-0148ccdeb877?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1901&q=80i&auto=format&fit=crop' },
+  { id: 5, url: Arena },
   { id: 1, url: 'https://images.unsplash.com/photo-1540206395-68808572332f?ixlib=rb-1.2.1&w=1181&q=80&auto=format&fit=crop' },
-  { id: 2, url: Arena },
-  { id: 3, url: AliFight },
   { id: 4, url: AliWin },
-  { id: 5, url: Alisster },
 ]
 
 const Slidehow = () => {
@@ -22,12 +22,12 @@ const Slidehow = () => {
     leave: { opacity: 0 },
     config: config.molasses,
   })
-  useEffect(() => void setInterval(() => set(state => (state + 1) % 6), 7000), [])
+  useEffect(() => void setInterval(() => set(state => (state + 1) % 6), 2000), [])
   return transitions?.map(({ item, props, key }) => (
     <animated.div
       key={key}
       class="bg"
-      style={{ ...props, backgroundImage: "url(" + ""+item.url+"" + ")" }}
+      style={{ ...props, backgroundImage: "url(" + item.url + ")" }}
     />
   ))
 }
