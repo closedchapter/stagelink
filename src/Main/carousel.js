45d4c262cf17c394dbba5102668dcb872b265e0c
Your Name <harrison.november@icloud.com>
2021-08-20 18:51:45 +0100

    go agian

--- src/Main/carousel.js
+++ src/Main/carousel.js
@@ -4,14 +4,18 @@ import Alisster from './images/alisster.png'
 import AliFight from './images/bgimg.jpg'
 import AliWin from './images/hero.jpg'
 import BlackGuy from './images/imagery.jpg'
+import LoganFight from './images/LoganFight.jpeg'
+import Pacquiao from './images/Pacquiao.jpeg'
+import ConorVsKhabib from './images/ConorVsKhabib.jpeg'
 
 const slides = [
   { id: 0, url: AliFight },
-  { id: 3, url: BlackGuy },
-  { id: 2, url: 'https://images.unsplash.com/photo-1544511916-0148ccdeb877?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1901&q=80i&auto=format&fit=crop' },
-  { id: 5, url: Alisster },
-  { id: 1, url: 'https://images.unsplash.com/photo-1540206395-68808572332f?ixlib=rb-1.2.1&w=1181&q=80&auto=format&fit=crop' },
-  { id: 4, url: AliWin },
+  { id: 1, url: BlackGuy },
+  { id: 2, url: AliWin },
+  { id: 3, url: Alisster },
+  { id: 4, url: LoganFight },
+  { id: 5, url: Pacquiao },
+  { id: 6, url: ConorVsKhabib },
 ]
 
 const Slidehow = () => {
@@ -22,7 +26,7 @@ const Slidehow = () => {
     leave: { opacity: 0 },
     config: config.molasses,
   })
-  useEffect(() => void setInterval(() => set(state => (state + 1) % 6), 7000), [])
+  useEffect(() => void setInterval(() => set(state => (state + 1) % 7), 4000), [])
   return transitions?.map(({ item, props, key }) => (
     <animated.div
       key={key}
