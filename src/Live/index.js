a316fe3502551016fa5b4855c580b2b51d14f1c5
Your Name <harrison.november@icloud.com>
2021-08-23 00:46:49 +0100

    FINAL PUSH

--- src/Live/index.js
+++ src/Live/index.js
@@ -5,7 +5,7 @@ const Landing = () => {
         <div className='bg-black flex h-screen'>
             <div className='animate__animated animate__fadeIn animate__slower animate__delay-5s my-auto mx-auto text-white text-3xl text-center font-black z-20 leading-tight filter brightness-105 opacity-80 neonText'>COMING SOON</div>
             <div className='absolute inset-0 z-10 bg-gradient-to-b from-transparent to-black mt-16 animate__animated animate__fadeIn animate__slower animate__delay-5s'/>
-            <video className='animate__animated animate__fadeIn animate__slower animate__delay-1s absolute h-full object-cover z-0' autoPlay loop muted playsInline>
+            <video className='animate__animated animate__fadeIn animate__slower animate__delay-1s absolute h-full object-cover z-0' playsInline loop autoPlay={true} allow="autplay" muted={true}>
                 <source src={require("./videos/boxer-sparring.mp4").default} type="video/mp4"/>
                 Your broswer does not support the video tag.
             </video> 
