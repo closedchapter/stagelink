f70ebb4f8a45517232924bf4fb34019a0e5deb01
horridharry <79926700+horridharry@users.noreply.github.com>
2021-08-30 21:25:04 +0100

    Create app.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='flex align-center justify-center h-screen'>
+<div className='font-normal text-base'>404! Web server has been temporarily suspended. Please retry with a different payment method.</div>
+    </div>
+  );
+}
+
+export default App;
