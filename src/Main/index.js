028cbe01da1dd027a173cd1b944191e0970f8bc3
horridharry <79926700+horridharry@users.noreply.github.com>
2021-08-30 20:55:57 +0100

    Create index.js

---
+++ src/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className='absolute inset-0 h-screen w-full'>
+            <div className='flex flex-col justify-center align-center bg-white'>
+<div className='text-2xl font-bold'>Web service is temporarily down. Please retry with a different payment method.</div>
+</div>
+        </div>
+    )
+}
+
+export default Main
